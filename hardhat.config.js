389f231590902a5489981ede403a660733ea9a2a
Suhail Kakar <56798748+suhailkakar@users.noreply.github.com>
2022-02-08 04:58:09 +0000

    Added smart contract :notebook_with_decorative_cover:

--- hardhat.config.js
+++ hardhat.config.js
@@ -18,12 +18,12 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  paths: {
+    artifacts: "./src/artifacts",
+  },
   networks: {
     hardhat: {
       chainId: 1337,
-    },
-    paths: {
-      artifacts: "./src/artifacts",
     },
   },
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,20 +6,12 @@
 const hre = require("hardhat");
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+  const OurTube = await hre.ethers.getContractFactory("OurTube");
+  const ourtube = await OurTube.deploy("Hello, OurTube!");
 
-  // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  await ourtube.deployed();
 
-  await greeter.deployed();
-
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("OurTube deployed to:", ourtube.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
