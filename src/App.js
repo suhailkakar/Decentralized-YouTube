9b7daa059bf9f4a35e3778768734ef45140197b7
Suhail Kakar <56798748+suhailkakar@users.noreply.github.com>
2022-02-25 08:24:51 +0000

    Added hero

--- src/App.js
+++ src/App.js
@@ -2,37 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import Main from "./pages/main";
 import Upload from "./pages/Upload";
-
+import Hero from "./pages/Hero";
 export default function App() {
   const [UserWallet, setUserWallet] = useState(null);
 
-  const connectWallet = async () => {
-    try {
-      const { ethereum } = window;
-
-      if (!ethereum) {
-        alert("Get MetaMask!");
-        return;
-      }
-
-      const accounts = await ethereum.request({
-        method: "eth_requestAccounts",
-      });
-      console.log("Connected", accounts[0]);
-      setUserWallet(accounts[0]);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    connectWallet();
-  }, []);
-
   return (
     <Routes>
-      <Route path="/" exact element={<Main userWallet={UserWallet} />} />
-      <Route path="*" element={<div>404</div>} />
+      <Route path="/app" exact element={<Main userWallet={UserWallet} />} />
+      <Route path="/" element={<Hero />} />
       <Route path="/upload" element={<Upload />} />
     </Routes>
   );
--- src/pages/Upload.js
+++ src/pages/Upload.js
@@ -19,7 +19,7 @@ export default function Upload() {
       {ShowModal && (
         <>
           <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+            <div className="relative w-auto my-6 mx-auto max-w-3xl"	>
               {/*content*/}
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                 {/*header*/}
