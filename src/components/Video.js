636931c8b47e57dff5bf3a127ac969e1ae07e158
Salman Dabbakuti <dabbakutisalman@gmail.com>
2022-06-25 11:49:49 +0530

    updated: relative timestamps, few UI fixes

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { BiCheck } from "react-icons/bi";
+import moment from "moment";
 
 export default function Video({ horizontal, video }) {
   return (
     <div
-      className={`${
-        horizontal
-          ? "flex flex-row mx-5 mb-5  item-center justify-center"
-          : "flex flex-col m-5"
-      } `}
+      className={`${horizontal
+        ? "flex flex-row mx-5 mb-5  item-center justify-center"
+        : "flex flex-col m-5"
+        } `}
     >
       <img
         className={
@@ -23,13 +23,12 @@ export default function Video({ horizontal, video }) {
         <h4 className="text-md font-bold dark:text-white mt-3 text-black">
           {video.title}
         </h4>
-        {horizontal && (
-          <p className="text-sm flex items-center text-textSubTitle mt-1">
-            {video.category} • March 7, 2022
-          </p>
-        )}
         <p className="text-sm flex items-center text-textSubTitle mt-1">
-          {horizontal ? null : video.category + " • "}
+          {video.category +
+            " • " +
+            moment(video.createdAt * 1000).fromNow()}
+        </p>
+        <p className="text-sm flex items-center text-textSubTitle mt-1">
           {video?.author?.slice(0, 9)}...{" "}
           <BiCheck size="20px" color="green" className="ml-1" />
         </p>
